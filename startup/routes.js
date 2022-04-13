3813870b1081ed34e8f603faa4fca6c056ceefad
Zhomart Suleymen <zhomart200275@gmail.com>
2022-04-14 02:56:36 +0600

    debug router err

--- startup/routes.js
+++ startup/routes.js
@@ -4,9 +4,9 @@ const sudokuRouter = require("../routes/sudoku");
 const path = require("path");
 
 module.exports = (app) => {
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../", "public/index.html"));
-    });
     app.use("/users", usersRouter);
     app.use("/sudoku", sudokuRouter);
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../", "public/index.html"));
+    });
 };
