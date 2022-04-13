0cb55b50beaa48b328607e4bef109b1d39a493ca
Zhomart Suleymen <zhomart200275@gmail.com>
2022-04-14 02:46:41 +0600

    debug routes

--- startup/routes.js
+++ startup/routes.js
@@ -4,7 +4,7 @@ const sudokuRouter = require("../routes/sudoku");
 const path = require("path");
 
 module.exports = (app) => {
-    app.use("/", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../", "public/index.html"));
     });
     app.use("/users", usersRouter);
