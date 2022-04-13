9247b54738a7b5c4d339761ae39b8b3d880d2958
Zhomart Suleymen <zhomart200275@gmail.com>
2022-04-14 02:32:24 +0600

    build add

--- startup/middlewares.js
+++ startup/middlewares.js
@@ -3,14 +3,13 @@ const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const path = require('path');
+
 module.exports = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(helmet());
     app.use(cors());
     app.use(cookieParser());
-    app.use((req, res, next) => {
-        console.log("request");
-        next();
-    });
+    app.use(express.static(path.join(__dirname, '../', 'public')));
 };
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,12 @@
-const usersRouter = require('../routes/users');
-const sudokuRouter = require('../routes/sudoku');
+const usersRouter = require("../routes/users");
+const sudokuRouter = require("../routes/sudoku");
+
+const path = require("path");
 
 module.exports = (app) => {
+    app.use("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../", "public/index.html"));
+    });
     app.use("/users", usersRouter);
     app.use("/sudoku", sudokuRouter);
 };
